b5dfdde0e9189dad619e02a2e4d1e181bc0d5f8a
gufin <gufinhaly@gmail.com>
2023-01-19 16:47:40 +0300

    persons and genres endpoints were added. films refactored

---
+++ fastapi-solution/src/api/v1/genres.py
@@ -0,0 +1,45 @@
+from http import HTTPStatus
+
+from fastapi import APIRouter, Depends, HTTPException, Query
+from pydantic import BaseModel
+
+from services.genre import GenreService, get_genre_service
+from models.models import UUIDMixin
+
+router = APIRouter()
+
+
+class GenreAPI(UUIDMixin, BaseModel):
+    name: str
+
+
+@router.get('/', response_model=list[GenreAPI])
+async def genre_list(
+        page_size: int = Query(10, description='Number of films on page'),
+        page: int = Query(1, description='Page number'),
+        sort: str = Query('',
+                          description='Sorting field. '
+                                      'Example: imdb_rating:desc'),
+        genre: str = Query(None, description='Filter by genre uuid'),
+        get_genre_service: GenreService = Depends(get_genre_service)
+) -> list[GenreAPI]:
+    genres = await get_genre_service.get_list(page_size=page_size,
+                                              page=page,
+                                              sort=sort,
+                                              genre=genre)
+    return [GenreAPI.parse_obj(genre.dict(by_alias=True)) for genre in
+            genres]
+
+
+@router.get('/{genre_id}', response_model=GenreAPI)
+async def genre_details(genre_id: str,
+                        genre_service: GenreService = Depends(
+                            get_genre_service)
+                        ) -> GenreAPI:
+    genre = await genre_service.get_by_id(genre_id)
+    if not genre:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
+                            detail='genre not found')
+
+    genre_api = GenreAPI.parse_obj(genre.dict(by_alias=True))
+    return genre_api
--- fastapi-solution/src/main.py
+++ fastapi-solution/src/main.py
@@ -6,7 +6,7 @@ from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
-from api.v1 import films
+from api.v1 import films, persons, genres
 from core import config
 from core.logger import LOGGING
 from db import elastic, redis
@@ -32,9 +32,9 @@ async def shutdown():
     await elastic.es.close()
 
 
-# Подключаем роутер к серверу, указав префикс /v1/films
-# Теги указываем для удобства навигации по документации
 app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
+app.include_router(persons.router, prefix='/api/v1/persons', tags=['persons'])
+app.include_router(genres.router, prefix='/api/v1/genres', tags=['genres'])
 
 if __name__ == '__main__':
     uvicorn.run(
